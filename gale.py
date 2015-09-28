12cdffd24a9005974b72d282c4daefea4c2a4abf
telezoic <daniel.sifton@viu.ca>
2015-09-28 14:08:08 -0700

    added gale

---
+++ gale.py
@@ -0,0 +1,20 @@
+#gale
+
+
+for url in urls:
+
+	r = requests.get(url[1])
+
+	soup = BeautifulSoup(r.text)
+		
+	if soup.find_all(text = re.compile("Table of Contents")):
+ 		print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
+ 		outurls.writerow([url[0], url[1], "Right On!"])
+ 		count += 1
+ 	else: 
+		print str(count) +  " of " + str(num_lines) + " | " + "Look into this . . . " + " | " + url[1]  + " | " + url[0]
+		outurls.writerow([url[0], url[1], "Look into this . . ."])  
+		count += 1
+
+
+
